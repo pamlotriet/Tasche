b04e8a5f1afb559501a733aa02b052d9d9e9f658
NightlyDarkFury <pamela.lotriet@gmail.com>
2023-07-07 05:04:31 +0200

    Changed menubar color on scroll

--- tasche/src/pages/main-page/main-page.component.ts
+++ tasche/src/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -10,14 +10,37 @@ export class MainPageComponent implements OnInit {
   navbarfixed:boolean = false;
   music : any;
 
+  sectionColors = [
+    { section: 'home', color: '#C17E27' },
+    { section: 'about', color: '#ffffff' },
+    { section: 'gigs', color: '#C17E27' },
+    { section: 'contact', color: '#ffffff' }
+  ];
+
   @HostListener('window:scroll',['$event']) onscroll(){
     if(window.scrollY > 100){
       this.navbarfixed = true;
     }else{
       this.navbarfixed = false;
     }
+
+    const menuBar = document.getElementById('nav');
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // Loop through the section colors array to find the current section
+    for (let i = this.sectionColors.length - 1; i >= 0; i--) {
+      const section = document.getElementById(this.sectionColors[i].section);
+      if(section !== null){
+        console.log(section)
+        if (section.offsetTop <= scrollTop) {
+          // Set the menu bar color based on the current section's background color
+          this.renderer.setStyle(menuBar, 'background-color', this.sectionColors[i].color);
+          break;
+        }
+      }
+    }
   }
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit(): void {
   }
